649b5ae2737d7d914686e5252e7a07bfd3c7c243
RilexBTW <101215214+RilexBTW@users.noreply.github.com>
2025-05-10 02:36:40 -0400

    Additions to config comments

--- config.lua
+++ config.lua
@@ -3,18 +3,18 @@ Config = {
 
 	System = {
 		Debug = false,
-		Notify = "gta",
-		drawText = "gta",
+		Notify = "gta", -- gta, ox, qb, esx, okok, red
+		drawText = "gta", -- gta, ox, qb, esx, red
 	},
 
 	General = {
 		ShowTrainBlips = true,			-- Show nearby train blips
-		showStationBlips = true,
+		showStationBlips = true, 		-- Show train station blips
 		requireMetroTicket = true,		-- Do metro trains require tickets
 		chargeBank = true,				-- charge players bank account
 		chargeAmount = 100,				-- amount per ticket
 		societyAccount = "cityhall",	-- Society account to fund when
 
-		seatPlayer = true,				-- Forcibly seat player in train saet, if false they are allowed to walk around
+		seatPlayer = true,				-- Forcibly seat player in train seat, if false they are allowed to walk around
 	},
 }
